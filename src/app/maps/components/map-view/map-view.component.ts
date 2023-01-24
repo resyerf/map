a780b7954fb7cd019544d4f6ef1cf99585c6760c
Freyser Ipanaque <fipanaque@canvia.com>
2023-01-24 15:03:35 -0500

    Creacion servicio de regresar a mi localizacionQ

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
 import { Map, Popup, Marker } from 'mapbox-gl';
+import { MapService } from '../../services/map.service';
 
 @Component({
   selector: 'app-map-view',
@@ -12,7 +13,10 @@ export class MapViewComponent implements AfterViewInit {
   @ViewChild('mapDiv')
   mapDivElement!:ElementRef
 
-  constructor( private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private mapService: MapService
+    ) { }
 
   ngAfterViewInit(): void {
     const map = new Map({
@@ -32,6 +36,9 @@ export class MapViewComponent implements AfterViewInit {
     .setLngLat(this.placesService.useLocation!)
     .setPopup(popup)
     .addTo(map)
+
+    this.mapService.setMap(map);
+
   }
 
 }
